9d48cb3815215823633ebf4595b757ea8ba4c17f
tokijh <tokijh@naver.com>
2019-05-13 22:17:10 +0900

    Initial Commit

---
+++ JHPinterestFlowLayout/JHPinterestFlowLayout.h
@@ -0,0 +1,19 @@
+//
+//  JHPinterestFlowLayout.h
+//  JHPinterestFlowLayout
+//
+//  Created by 윤중현 on 13/05/2019.
+//  Copyright © 2019 tokijh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JHPinterestFlowLayout.
+FOUNDATION_EXPORT double JHPinterestFlowLayoutVersionNumber;
+
+//! Project version string for JHPinterestFlowLayout.
+FOUNDATION_EXPORT const unsigned char JHPinterestFlowLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JHPinterestFlowLayout/PublicHeader.h>
+
+
